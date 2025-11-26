d5d87b0bffaf533cbd83b01c20d315cb1a200da0
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-11-26 20:20:33 -0300

    feat: add zone entities

--- src/zones/entities/zone.entity.ts
+++ src/zones/entities/zone.entity.ts
@@ -1,1 +1,28 @@
-export class Zone {}
+import { BeforeInsert, Column, Entity, PrimaryColumn } from 'typeorm';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const { nanoid } = require('nanoid');
+
+@Entity()
+export class Zone {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    type: string;
+
+    @Column()
+    geometry: string;
+
+    @Column()
+    createdAt: string;
+
+    @BeforeInsert()
+    generateId() {
+        this.id = nanoid();
+    }
+}
